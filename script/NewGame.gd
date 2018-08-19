6acd3faea60478617f4ec9d27910edb99f402511
Unknown <sly.pims@gmail.com>
2018-08-19 16:49:05 +1200

    Menu
    Addition of main menu with lobby music as well as pause menu. Added different music to arena1

---
+++ script/NewGame.gd
@@ -0,0 +1,15 @@
+extends Button
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+       get_tree().change_scene("res://scene/Arena1.tscn")
+
---
+++ script/PausePopup.gd
@@ -0,0 +1,23 @@
+extends Popup
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _input(event):
+#	if event.type == InputEventKey and event.scancode == KEY_ESCAPE:
+	if event.is_pressed() and event.scancode == KEY_ESCAPE:
+		get_tree().paused = true
+		get_parent().get_node("MainGameMusic").stop()
+		get_parent().get_node("pause_popup/PauseMusic").play()
+		get_parent().get_node("pause_popup/WindowDialog").show()
+	if event.is_pressed() and event.scancode == KEY_ENTER or event.is_pressed() and event.scancode == KEY_KP_ENTER:
+		get_tree().paused = false
+		get_parent().get_node("MainGameMusic").play()
+		get_parent().get_node("pause_popup/PauseMusic").stop()
+		get_parent().get_node("pause_popup/WindowDialog").hide()
